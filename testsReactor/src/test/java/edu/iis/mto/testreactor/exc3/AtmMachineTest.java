616dd456193cd7e06ff77a4897b3c2e4834a8f15
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-05-16 16:46:06 +0200

    added test should throw exception if amount is grather or equals zero

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -3,13 +3,43 @@ package edu.iis.mto.testreactor.exc3;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AtmMachineTest {
+
+    @Mock
+    BankService bankService;
+    @Mock
+    CardProviderService cardProviderService;
+    @Mock
+    MoneyDepot moneyDepot;
+    AtmMachine atmMachine;
+    Money money;
+    Card card;
+
+    @Before
+    public void setup(){
+        atmMachine = new AtmMachine(cardProviderService,bankService,moneyDepot);
+        money = Money.builder().withAmount(10).withCurrency(Currency.PL).build();
+        card = Card.builder().withCardNumber("TEST").withPinNumber(1234).build();
+    }
+
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
+    @Test(expected = WrongMoneyAmountException.class)
+    public void shouldThrowWrongMoneyAmountException(){
+
+        Money moneyForTestScope = Money.builder().withAmount(-10).withCurrency(Currency.PL).build();
+        atmMachine.withdraw(moneyForTestScope, card);
+
+    }
 }
