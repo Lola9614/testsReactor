313b1bad756f6988ee5ed1e3528014bc8d8cd1ca
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-05-16 17:21:33 +0200

    added test shouldRunInProperOrderServices

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -3,15 +3,19 @@ package edu.iis.mto.testreactor.exc3;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AtmMachineTest {
@@ -26,11 +30,23 @@ public class AtmMachineTest {
     Money money;
     Card card;
 
+    @Captor
+    ArgumentCaptor<List<Banknote>> banknoteList;
+
+    @Captor
+    ArgumentCaptor<AuthenticationToken> authenticationTokenGlobal;
+
+    @Captor
+    ArgumentCaptor<Card> cardArgumentCaptor;
+
+    AuthenticationToken authenticationToken;
+
     @Before
     public void setup(){
         atmMachine = new AtmMachine(cardProviderService,bankService,moneyDepot);
         money = Money.builder().withAmount(10).withCurrency(Currency.PL).build();
         card = Card.builder().withCardNumber("TEST").withPinNumber(1234).build();
+        authenticationToken = AuthenticationToken.builder().withUserId("1").withAuthorizationCode(123).build();
     }
 
 
@@ -49,8 +65,25 @@ public class AtmMachineTest {
     @Test(expected =CardAuthorizationException.class)
     public void shouldThrowExceptionIfCardAuthorizationFailed(){
 
-        Mockito.when(cardProviderService.authorize(Mockito.any())).thenReturn(Optional.empty());
+        when(cardProviderService.authorize(Mockito.any())).thenReturn(Optional.empty());
         atmMachine.withdraw(money,card);
+    }
+
+    @Test
+    public void shouldRunServicesInProperOrderIfAllIsGood(){
+
+
+        when(cardProviderService.authorize(Mockito.any())).thenReturn(Optional.of((authenticationToken)));
+        when(bankService.charge(Mockito.any(),Mockito.any())).thenReturn(true);
+        when(moneyDepot.releaseBanknotes(Mockito.anyList())).thenReturn(true);
+
+        atmMachine.withdraw(money,card);
+
+        InOrder inOrder = Mockito.inOrder(bankService, cardProviderService, moneyDepot);
+        inOrder.verify(cardProviderService).authorize(card);
+        inOrder.verify(moneyDepot).releaseBanknotes(banknoteList.capture());
+        inOrder.verify(bankService).commit(authenticationTokenGlobal.capture());
+
     }
 
 }
